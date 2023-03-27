b45536b092b712a276245fe98d0c850768ca4808
Leah <leahxharden@gmail.com>
2023-03-27 16:28:05 -0400

    fix bug

--- db/schema.sql
+++ db/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE role_employee (
     ON DELETE SET NULL
 )
 CREATE TABLE employee (
-    id: INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id: INT NOT NULL AUTO_INCREMENT,
     first_name: VARCHAR(30),
     last_name: VARCHAR(30),
     role_id: INT,
--- server.js
+++ server.js
@@ -18,9 +18,9 @@ const db = mysql.createConnection(
         user: 'root',
         // TODO: Add MySQL password here
         password: process.env.password,
-        database: 'movies_db'
+        database: 'tracker_db'
     },
-    console.log(`Connected to the movies_db database.`)
+    console.log(`Connected to the tracker_db database.`)
 );
 
 // Connect to database  ----------
