37fd9aed1b65fbd381f64505a001c92bae7f23b8
Leah <leahxharden@gmail.com>
2023-03-28 19:43:47 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,23 @@
 INSERT INTO departments (name)
-VALUES (,"Lion King"),
-    ("The Godfather"),
-    ("West Side Story"),
-    ("Parasite"),
-    ("The Wizard of Oz");
+VALUES ("Human Resources"),
+    ("IT"),
+    ("Accounting"),
+    ("Marketing"),
+    ("R&D");
 
 INSERT INTO role_employee (title, salary)
-VALUES (, "Zazu is underrated. Give that hornbill a sequel!","$7.75"),
-    ( "I'm gonna make him an offer you can't refuse, watch this movie","$9.75"),
-    ( "Scar is the lion everyone loves to hate","$9.75"),
-    ( "Ten years of ballet and three years of tap to join a gang in this neighborhood","$9.75"),
-    ( "The tin man gave a metallic, hollow performance","$9.75"),
-    ( "Hakuna matata","$9.75"),
-    ( "Those flying monkeys are nightmare fuel!","$9.75");
+VALUES (, "Manager","$7.75"),
+    ( "Marketing Coordinator","$9.75"),
+    ( "Public Relations","$20.00"),
+    ( "Receptionist","$15.50"),
+    ( "Secretary","$9.75"),
+    ( "COO—Chief Operating Officer","$14.25"),
+    ( "TComputer Scientist","$22.75");
     
 
-INSERT INTO employee (id,first_name,manager_id)
-VALUES (,"Dave","Smith",1),
-    ("Cara","Smith",1),
-    ("Page","Jone",1),
-    ("Dave","Kite",1),
-    ("Cate","Smith",1),
+INSERT INTO employee (first_name,last_name)
+VALUES (,"Dave","Smith"),
+    ("Cara","Smith",),
+    ("Page","Jone"),
+    ("Dave","Kite"),
+    ("Cate","Smith"),
