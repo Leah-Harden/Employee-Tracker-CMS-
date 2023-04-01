cbd35f4b4a4a7f0a83b499380ef0f48a10db0cbd
Leah <leahxharden@gmail.com>
2023-03-31 23:43:33 -0400

    update db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const env = require('dotenv');
+require('dotenv').config;
 //require  ----------
 
 // Connect to database  ----------
